686bb5b71d2d7b2e83a2eace8f44aebf4ab7d119
shoptorshichowdhury <shoptorshichowdhury.info@gmail.com>
2025-02-12 19:30:13 +0600

    light/dark theme slightly changed

--- src/components/DarkMood/DarkMode.jsx
+++ src/components/DarkMood/DarkMode.jsx
@@ -1,4 +1,21 @@
+import { useEffect, useState } from "react";
+
 const DarkMood = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  useEffect(() => {
+    const systemPrefersDark = window.matchMedia(
+      "(prefers-color-scheme: dark)"
+    ).matches;
+    const selectedTheme = localStorage.getItem("selectedTheme");
+
+    if (selectedTheme) {
+      setIsDarkMode(selectedTheme === "dark");
+    } else {
+      setIsDarkMode(systemPrefersDark);
+    }
+  }, []);
+
   const setDarkMode = () => {
     document.querySelector("body").setAttribute("data-theme", "dark");
     localStorage.setItem("selectedTheme", "dark");
@@ -9,28 +26,21 @@ const DarkMood = () => {
     localStorage.setItem("selectedTheme", "light");
   };
 
-  const selectedTheme = localStorage.getItem("selectedTheme");
-  if (selectedTheme === "dark") {
-    setDarkMode();
-  }
-
   const toggleTheme = (e) => {
-    if (e.target.checked) setDarkMode();
-    else setLightMode();
+    const newTheme = e.target.checked ? "dark" : "light";
+    setIsDarkMode(e.target.checked);
+    newTheme === "dark" ? setDarkMode() : setLightMode();
   };
 
   return (
     <div>
       <label className="swap swap-rotate">
-        {/* this hidden checkbox controls the state */}
         <input
           type="checkbox"
           className="theme-controller"
           onChange={toggleTheme}
-          defaultChecked={selectedTheme === "dark"}
+          checked={isDarkMode}
         />
-
-        {/* sun icon */}
         <svg
           className="swap-on h-5 w-5 md:h-8 md:w-8 fill-current"
           xmlns="http://www.w3.org/2000/svg"
@@ -39,7 +49,6 @@ const DarkMood = () => {
           <path d="M5.64,17l-.71.71a1,1,0,0,0,0,1.41,1,1,0,0,0,1.41,0l.71-.71A1,1,0,0,0,5.64,17ZM5,12a1,1,0,0,0-1-1H3a1,1,0,0,0,0,2H4A1,1,0,0,0,5,12Zm7-7a1,1,0,0,0,1-1V3a1,1,0,0,0-2,0V4A1,1,0,0,0,12,5ZM5.64,7.05a1,1,0,0,0,.7.29,1,1,0,0,0,.71-.29,1,1,0,0,0,0-1.41l-.71-.71A1,1,0,0,0,4.93,6.34Zm12,.29a1,1,0,0,0,.7-.29l.71-.71a1,1,0,1,0-1.41-1.41L17,5.64a1,1,0,0,0,0,1.41A1,1,0,0,0,17.66,7.34ZM21,11H20a1,1,0,0,0,0,2h1a1,1,0,0,0,0-2Zm-9,8a1,1,0,0,0-1,1v1a1,1,0,0,0,2,0V20A1,1,0,0,0,12,19ZM18.36,17A1,1,0,0,0,17,18.36l.71.71a1,1,0,0,0,1.41,0,1,1,0,0,0,0-1.41ZM12,6.5A5.5,5.5,0,1,0,17.5,12,5.51,5.51,0,0,0,12,6.5Zm0,9A3.5,3.5,0,1,1,15.5,12,3.5,3.5,0,0,1,12,15.5Z" />
         </svg>
 
-        {/* moon icon */}
         <svg
           className="swap-off h-5 w-5 md:h-8 md:w-8 fill-current"
           xmlns="http://www.w3.org/2000/svg"
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -4,11 +4,11 @@ import errorPage from "../assets/error-page.jpg";
 const ErrorPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen space-y-2 md:space-y-5">
+      <h2 className="text-lg md:text-4xl font-bold">Page Not Found</h2>
       <div className="w-[200px] h-[200px] md:w-[300px] md:h-[300px] lg:w-[400px] lg:h-[400px] ">
         <img src={errorPage} alt="error" />
       </div>
       <div className="text-center">
-        <h2 className="text-lg md:text-5xl font-bold mb-4 md:mb-8">Page Not Found</h2>
         <Link to="/">
           <button className="btn btn-info">Get Back Home</button>
         </Link>
